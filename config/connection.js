2812ee485a87eb33d0d5d0904bd06aa44c1e0be7
Greg Scott <gtscott90@gmail.com>
2021-02-18 18:02:21 -0500

    connection.js - connection set up and exported

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// Set up connection to SQL
+const mysql = require('mysql');
+require('dotenv').config();
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: process.env.PASS,
+  database: 'burger_db',
+});
+
+// connect to SQL
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+// Export connection for the ORM 
+module.exports = connection;
