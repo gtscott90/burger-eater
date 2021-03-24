7183eab3ebe2964c41843deea09d6a12f70ee5d5
Greg Scott <gtscott90@gmail.com>
2021-03-23 23:30:44 -0400

    api route for updating a burger status to devoured added

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -14,9 +14,33 @@ router.get('/', (req, res) => {
     });
   });
 
+  // creating new burger... sends data to the DB
   router.post('/api/burgers', (req, res) => {
     burger.create(['name', 'devoured'], [req.body.name, req.body.devoured], (result) => {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
   });
+
+  // updates the status of a burger to devoured
+  router.put('/api/burgers/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+  
+    console.log('condition', condition);
+  
+    burger.update(
+      {
+        devoured: true,
+      },
+      condition,
+      (result) => {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+      }
+    );
+  });
+
+  module.exports = router;
